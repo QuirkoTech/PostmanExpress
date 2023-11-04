6abc22c0ea8ed61a9f80d5cfcf8d0f306a4df5b2
Matheus-OAMK <t2fema00@students.oamk.fi>
2023-11-04 13:48:23 +0200

    Postgress Schema for all tables, datatypes

---
+++ db.sql
@@ -0,0 +1,117 @@
+DROP DATABASE IF EXISTS postman_express_db;
+
+CREATE DATABASE postman_express_db;
+
+USE postman_express_db;
+
+DROP TABLE users;
+
+DROP TABLE parcels;
+
+DROP TABLE cabinets;
+
+DROP TABLE user_parcels;
+
+DROP TABLE drivers;
+
+DROP TABLE driver_parcels;
+
+DROP TYPE PARCELSTATUS;
+
+DROP TYPE CABINETSTATUS;
+
+DROP TYPE LOCATION;
+
+DROP EXTENSION IF EXISTS "uuid-ossp";
+
+
+CREATE TYPE LOCATION AS ENUM (
+  'warehouse', -- central warehouse maybe like 100 cabinets?
+  'oulu', -- 15 cabinets
+  'helsinki', -- 15 cabinets
+  'turku', -- 15 cabinets
+  'tampere', -- 15 cabinets
+  'espoo' -- 15 cabinets
+);
+
+CREATE TYPE CABINETSTATUS AS ENUM (
+  'empty', -- Nothing inside
+  'reserved', -- Waiting from a parcel from customer or driver
+  'occupied', -- A parcel is inside ready for pickup from driver or customer
+);
+
+
+CREATE TYPE PARCELSTATUS AS ENUM {
+  'awaiting drop-off', -- After creating a new parcel & timestamp also used for when order is created
+  'Prepared for Delivery', -- Inside a cabinet, waiting for a driver
+  'en route to the warehouse', -- Driver accepted delivery, not yet in the final destination
+  'at warehouse', -- Parcel is at warehouse waiting for a driver to accept
+  'en route to the pickup location', -- Parcel is accepted and on the way to the final destination
+  'ready for pickup', -- In the final destination cabinet, waiting for the customer
+  'delivered', -- Customer picked up the parcel from the cabinet
+}
+
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
+
+CREATE TABLE users (
+  user_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  user_name varchar(255) NOT NULL,
+  user_email varchar(255) UNIQUE NOT NULL,
+  password varchar(255) NOT NULL,
+  refresh_token varchar(255),
+  user_location LOCATION NOT NULL
+);
+
+
+
+CREATE TABLE parcels (
+  parcel_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  parcel_status PARCELSTATUS NOT NULL,
+  parcel_sender_id UUID REFERENCES users(user_id),
+  parcel_receiver_email varchar(255),
+  height integer NOT NULL,
+  length integer NOT NULL,
+  width integer NOT NULL,
+  weight integer NOT NULL,
+  pickup_pin integer UNIQUE,
+  delivery_pin integer UNIQUE,
+  status_timestamps jsonb
+);
+
+
+CREATE TABLE cabinets (
+  cabinet_id SERIAL PRIMARY KEY,
+  cabinet_status CABINETSTATUS NOT NULL,
+  cabinet_location LOCATION NOT NULL,
+  parcel_id UUID REFERENCES parcels(parcel_id)
+);
+
+CREATE TABLE user_parcels (
+  id SERIAL PRIMARY KEY,
+  parcel_id UUID REFERENCES parcels(parcel_id),
+  notify BOOLEAN DEFAULT TRUE
+);
+
+
+CREATE TABLE drivers (
+  driver_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  driver_name varchar(255) NOT NULL,
+  driver_email varchar(255) UNIQUE NOT NULL,
+  password varchar(255) NOT NULL,
+  refresh_token varchar(255),
+  driver_location LOCATION NOT NULL
+);
+
+
+
+CREATE TABLE driver_parcels (
+  id SERIAL PRIMARY KEY,
+  driver_id UUID REFERENCES drivers(driver_id),
+  parcel_id UUID REFERENCES parcels(parcel_id),
+  notify BOOLEAN DEFAULT TRUE
+)
+
+
