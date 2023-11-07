641e216534f3ff687305ddaff037a0d86524ab25
RepoRover <web.yugaitrov@gmail.com>
2023-11-07 13:18:38 +0200

    Some additions

--- consumer_app/server/app.js
+++ consumer_app/server/app.js
@@ -1,23 +1,21 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import './config.js';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import "./config.js";
 
-import globalErrorHandler from './controllers/errorControllers.js';
-import APIError from './helpers/APIError.js';
+import globalErrorHandler from "./controllers/errorControllers.js";
+import APIError from "./helpers/APIError.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-if (process.env.ENV === 'dev') app.use(morgan('dev'));
+if (process.env.ENV === "dev") app.use(morgan("dev"));
 
 // Here are all the application routes
 
-
-
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
     next(new APIError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
