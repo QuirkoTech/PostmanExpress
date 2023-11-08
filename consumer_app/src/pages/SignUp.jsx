1407114ba54d215dd1319b69905d472a66ed2619
Matheus-OAMK <t2fema00@students.oamk.fi>
2023-11-08 20:37:18 +0200

    Signup shows error messagges. Form validation

--- consumer_app/src/pages/SignUp.jsx
+++ consumer_app/src/pages/SignUp.jsx
@@ -1,60 +1,175 @@
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { DevTool } from "@hookform/devtools";
+import { ChevronsUpDown } from "lucide-react";
+import { useEffect } from "react";
+import Input from "../components/Input";
+
+let renderCount = 0;
 
 const SignUp = () => {
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors, isSubmitSuccessful },
+        isValidating,
+        watch,
+        setFocus,
+        reset,
+    } = useForm({ mode: "onTouched", defaultValues: { location: "" } });
+
+    const navigate = useNavigate();
+    const password = watch("password");
+
+    useEffect(() => {
+        if (isSubmitSuccessful) {
+            reset();
+            navigate("/login");
+        }
+    }, [isSubmitSuccessful, reset, navigate]);
+
+    renderCount++;
+    console.log(isSubmitSuccessful);
+
+    const submitHandler = (data) => {
+        console.log(data);
+    };
+
+    useEffect(() => {
+        setFocus("name");
+    }, [setFocus]);
+
+    const locationValue = watch("location");
     return (
         <section className="padding">
             <div className="max-container">
                 <h1 className="mt-14 text-center text-5xl text-white ">
-                    PostmanExpress
+                    PostmanExpress {renderCount}
                 </h1>
                 <div className="mx-auto w-64">
                     <form
+                        onSubmit={handleSubmit(submitHandler)}
                         action=""
-                        className="flex flex-col items-center text-lg"
+                        className="gap flex flex-col items-center text-lg"
+                        noValidate
                     >
                         <h2 className=" pb-5 pt-20 text-lg text-white ">
                             Customer Sign Up
                         </h2>
-                        <input
+                        <Input
                             type="text"
-                            className="bg-dark-secondary rounded-low border-slate-blue mb-6 w-full border-2 px-5 py-1 focus:outline-none focus:ring-1"
-                            placeholder="Full Name"
-                            name="name"
+                            placeHolder="Full Name"
+                            register={{
+                                ...register("name", {
+                                    required: {
+                                        value: true,
+                                        message: "This field is required",
+                                    },
+                                }),
+                            }}
+                            errorMessage={errors.name?.message}
                         />
-                        <input
+                        <Input
                             type="email"
-                            className="bg-dark-secondary rounded-low border-slate-blue mb-6 w-full border-2 px-5 py-1 focus:outline-none focus:ring-1"
-                            placeholder="Email"
-                            name="email"
+                            placeHolder="Email"
+                            register={{
+                                ...register("email", {
+                                    pattern: {
+                                        value: /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]{2,}$/,
+                                        message: "Invalid email format",
+                                    },
+                                    required: {
+                                        value: true,
+                                        message: "This field is required",
+                                    },
+                                    // custom validation need to replace with our own api
+                                    validate: async (value) => {
+                                        const reponse = await fetch(
+                                            `https://jsonplaceholder.typicode.com/users?email=${value}`,
+                                        );
+                                        const data = await reponse.json();
+                                        return (
+                                            data.length == 0 ||
+                                            "This email is already registered"
+                                        );
+                                    },
+                                }),
+                            }}
+                            errorMessage={errors.email?.message}
                         />
-                        <input
-                            type="text"
-                            className="bg-dark-secondary rounded-low border-slate-blue mb-6 w-full border-2 px-5 py-1 focus:outline-none focus:ring-1"
-                            placeholder="Password"
-                            name="password"
+
+                        <Input
+                            type="password"
+                            placeHolder="Password"
+                            register={{
+                                ...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: "This field is required",
+                                    },
+                                    minLength: {
+                                        value: 7,
+                                        message:
+                                            "Password must be at least 7 characters",
+                                    },
+                                }),
+                            }}
+                            errorMessage={errors.password?.message}
                         />
-                        <input
-                            type="text"
-                            className="bg-dark-secondary rounded-low border-slate-blue mb-6 w-full border-2 px-5 py-1 focus:outline-none focus:ring-1"
-                            placeholder="Confirm Password"
-                            name="ConfirmPassword"
+
+                        <Input
+                            type="password"
+                            placeHolder="Confirm Password"
+                            register={{
+                                ...register("confirmPassword", {
+                                    required: {
+                                        value: true,
+                                        message: "This field is required",
+                                    },
+                                }),
+                            }}
+                            errorMessage={errors.confirmPassword?.message}
                         />
-                        <select
-                            name="location"
-                            id=""
-                            className="bg-dark-secondary rounded-low border-slate-blue focus:ring-1.5 mb-9 w-full border-2 px-5 py-1 focus:outline-none"
+
+                        <div className="relative w-full">
+                            <select
+                                {...register("location", {
+                                    required: {
+                                        value: true,
+                                        message: "This field is required",
+                                    },
+                                })}
+                                id=""
+                                className={`custom-select ${
+                                    locationValue === "" ? "opacity-70" : ""
+                                }`}
+                                autoComplete="off"
+                            >
+                                <option value="" hidden disabled>
+                                    Choose a location
+                                </option>
+                                <option value="oulu">Oulu</option>
+                                <option value="helsinki">Helsinki</option>
+                                <option value="turku">Turku</option>
+                                <option value="tampere">Tampere</option>
+                                <option value="espoo">Espoo</option>
+                            </select>
+                            <ChevronsUpDown className="stroke-slate-blue absolute right-2 top-2 " />
+                            <p className="text-danger-main mb-9 self-start text-sm">
+                                {errors.location?.message}
+                            </p>
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="rounded-low bg-green-main self-end px-7 py-1"
+                            disabled={isValidating}
                         >
-                            <option value="oulu">Oulu</option>
-                            <option value="helsinki">Helsinki</option>
-                            <option value="turku">Turku</option>
-                            <option value="tampere">Tampere</option>
-                            <option value="espoo">Espoo</option>
-                        </select>
-                        <button className="rounded-low bg-green-main self-end px-7 py-1">
                             Apply
                         </button>
                     </form>
+                    <DevTool control={control} />
                     <p className="mt-9 text-lg">
                         Already have an account?{" "}
                         <Link to="/login" className="text-white">
