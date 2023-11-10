134254a817eeacb851fc613d428e231866d6a26f
Matheus-OAMK <t2fema00@students.oamk.fi>
2023-11-10 13:54:19 +0200

    useform and yup integrated to consumer login app

--- consumer_app/src/pages/Login.jsx
+++ consumer_app/src/pages/Login.jsx
@@ -1,8 +1,84 @@
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
-import { Button } from "../components/button";
+import { Button, Input } from "../components";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useEffect } from "react";
+import { DevTool } from "@hookform/devtools";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+    // Yup validation schema for form validation
+    const schema = yup.object({
+        email: yup
+            .string()
+            .email("Email format is not valid")
+            .required("This field is required"),
+        password: yup.string().required("This field is required"),
+    });
+
+    const {
+        register,
+        handleSubmit,
+        control,
+        setError,
+        formState: { errors, isSubmitSuccessful, isSubmitting },
+        setFocus,
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    // Set focus on email input on page load
+    useEffect(() => {
+        setFocus("email");
+    }, [setFocus]);
+
+
+
+    // Filler handler for form submission for test purposes
+    const submitHandler = async (data) => {
+        try {
+            const response = await fetch("https://reqres.in/api/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: data.email,
+                    password: data.password,
+                }),
+            });
+
+            if (response.ok) {
+                // Successful response
+                return;
+            } else {
+                // Handle different HTTP status codes
+                if (response.status === 400) {
+                    setError("email", {
+                        type: "manual",
+                        message: "Invalid email or password",
+                    });
+                    console.log("set custom");
+                }
+            }
+        } catch (error) {
+            console.log("Error:", error);
+
+            // Additional error handling if needed
+        }
+    };
+
+    // // Reroute to home page after successful login
+    // const navigate = useNavigate();
+    // useEffect(() => {
+    //     if (isSubmitSuccessful) {
+    //         reset();
+    //         navigate("/");
+    //     }
+    // }, [isSubmitSuccessful, reset, navigate]);
+
+    console.log(isSubmitSuccessful);
     return (
         <section className="padding">
             <div className="max-container">
@@ -11,27 +87,32 @@ const Login = () => {
                 </h1>
                 <div className="mx-auto w-64">
                     <form
+                        onSubmit={handleSubmit(submitHandler)}
                         action=""
                         className="flex flex-col items-center text-lg"
+                        noValidate
                     >
                         <h2 className=" pb-5 pt-20 text-lg text-white ">
                             Customer Log In
                         </h2>
-                        <input
+                        <Input
                             type="email"
-                            className="bg-dark-secondary rounded-low border-slate-blue mb-6 w-full border-2 px-5 py-1 focus:outline-none focus:ring-1"
-                            placeholder="Email"
-                            name="email"
+                            placeHolder="Email"
+                            register={{
+                                ...register("email"),
+                            }}
+                            errorMessage={errors.email?.message}
                         />
-                        <input
-                            type="text"
-                            className="bg-dark-secondary rounded-low border-slate-blue mb-6 w-full border-2 px-5 py-1 focus:outline-none focus:ring-1"
-                            placeholder="Password"
-                            name="password"
+                        <Input
+                            type="password"
+                            placeHolder="Password"
+                            register={{ ...register("password") }}
+                            errorMessage={errors.password?.message}
                         />
 
-                    <Button></Button>
+                        <Button />
                     </form>
+                    <DevTool control={control} />
                     <p className="mt-9 whitespace-nowrap text-lg">
                         Don't have an account yet?{" "}
                         <Link to="/signup" className="text-white">
--- driver_app/src/pages/index.js
+++ driver_app/src/pages/index.js
@@ -1,7 +1,6 @@
 // ********* Use this file to import the different pages for cleaner imports inside the app *********
 
-import SignUp from "./SignUp";
 import Login from "./Login";
 import HomePage from "./HomePage";
 import NotFoundPage from "./NotFoundPage";
-export { SignUp, Login, HomePage, NotFoundPage };
+export { Login, HomePage, NotFoundPage };
