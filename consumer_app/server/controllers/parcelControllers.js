1f43bc8ec71a3202dcea5c27b5c1ca11e1498c37
RepoRover <web.yugaitrov@gmail.com>
2023-11-14 13:13:26 +0200

    Some additions

---
+++ consumer_app/server/controllers/parcelControllers.js
@@ -0,0 +1,4 @@
+import APIError from "./../helpers/APIError.js";
+import catchAsync from "./../helpers/catchAsync.js";
+
+export const newParcel = catchAsync(async (req, res, next) => {});
--- organization_api/server/routes/parcelRoutes.js
+++ organization_api/server/routes/parcelRoutes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import protect from "./../helpers/protect.js";
 
 const router = Router();
+
+router.route("/new").post(protect);
 
 export default router;
