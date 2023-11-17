2854a0ef717b525aeb99c3259f58fa3d42cead6e
RepoRover <web.yugaitrov@gmail.com>
2023-11-18 00:57:48 +0200

    Added env variables check

--- locker_app/server/app.js
+++ locker_app/server/app.js
@@ -1,7 +1,17 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import './config.js';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import "./config.js";
+
+if (
+    !process.env.ENV ||
+    !process.env.API_PROCESS_PORT ||
+    !process.env.API_KEY ||
+    !process.env.APP_HEADER
+) {
+    console.error("Missing required environment variables. Exiting...");
+    process.exit(1);
+}
 
 // import globalErrorHandler from './controllers/errorController.js';
 // import APIError from './helpers/APIError.js';
@@ -11,7 +21,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-if (process.env.ENV === 'dev') app.use(morgan('dev'));
+if (process.env.ENV === "dev") app.use(morgan("dev"));
 
 // app.all('*', (req, res, next) => {
 //     next(new APIError(`Can't find ${req.originalUrl} on this server!`, 404));
