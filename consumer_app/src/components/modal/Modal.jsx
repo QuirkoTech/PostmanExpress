e6afd7bded1bcd1204fbdcf0e7cd53d0b67e05d3
Matheus-OAMK <t2fema00@students.oamk.fi>
2023-11-20 16:22:49 +0200

    Logout for consumer app

--- consumer_app/src/components/modal/Modal.jsx
+++ consumer_app/src/components/modal/Modal.jsx
@@ -1,46 +1,80 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../auth";
 
 function Modal({ isOpen, closeModal, name, menuLinks, className }) {
+    const { fetchUser } = useContext(AuthContext);
+
+    // Backend URL for the consumer app
+    const CONSUMER_URL = import.meta.env.VITE_CONSUMER_BACKEND_URL;
+
+    //Signout function
+    const handleSignOut = async () => {
+        try {
+            const response = await axios.post(
+                `${CONSUMER_URL}/auth/logout`,
+                {},
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    withCredentials: true,
+                },
+            );
+
+            const message = response.data.status;
+
+            if (message === "success") {
+                fetchUser();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     if (!isOpen) {
         return null;
     }
 
     return (
-        <div className={` ${className}`}>
+        <nav className={` ${className}`}>
             <div className={`modal-container ${className}`}>
                 <ul className="border-b-2 border-solid border-[#494844]">
-                    <li className="text-lg font-medium w-full text-white pb-5 mb-4 border-b-2 border-solid border-[#494844]">
+                    <li className="mb-4 w-full border-b-2 border-solid border-[#494844] pb-5 text-lg font-medium text-white">
                         {name}
                         <button
                             onClick={closeModal}
-                            className="absolute top-4 right-2 p-2 px-4 bg-transparent transition-all text-white border-none cursor-pointer hover:text-red-500 focus:outline-none hover:border-none"
+                            className="absolute right-2 top-4 cursor-pointer border-none bg-transparent p-2 px-4 text-white transition-all hover:border-none hover:text-red-500 focus:outline-none"
                         >
-                            <FontAwesomeIcon className='w-5 h-5' icon={faXmark} />
+                            <FontAwesomeIcon
+                                className="h-5 w-5"
+                                icon={faXmark}
+                            />
                         </button>
                     </li>
                     {menuLinks.map((link, index) => (
-                        <li key={index} className="text-white text-lg font-normal my-5">
+                        <li
+                            key={index}
+                            className="my-5 text-lg font-normal text-white"
+                        >
                             <a className="text-white" href={link.url}>
                                 {link.title}
                             </a>
                         </li>
                     ))}
                 </ul>
-                <div className='flex flex-col '>
+                <div className="flex flex-col ">
                     <button
-                        // onClick={handleSignOut}
-                        className="mr-auto p-0 my-5 bg-transparent transition-all text-[#C55B5B] text-lg font-medium border-none cursor-pointer focus:outline-none hover:border-none"
+                        onClick={handleSignOut}
+                        className="my-5 mr-auto cursor-pointer border-none bg-transparent p-0 text-lg font-medium text-[#C55B5B] transition-all hover:border-none focus:outline-none"
                     >
                         Log Out
-        
                     </button>
-
-                
-
                 </div>
             </div>
-        </div>
+        </nav>
     );
 }
 
