e5077eb7e974c618a848ff05e85bf370fe399bdf
hha2907 <ha.dh290702@gmail.com>
2023-11-24 19:00:08 +0200

    User can add new parcel

--- consumer_app/src/components/layout/Layout.jsx
+++ consumer_app/src/components/layout/Layout.jsx
@@ -1,15 +1,20 @@
-import React, { Fragment } from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import React, { Fragment } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Layout = ({ children }) => {
     return (
-        <Fragment >
-            
-                <Header></Header>
-                {children}
-                <Footer></Footer>
-            
+        <Fragment>
+            <Header></Header>
+            {children}
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar
+            />
+            <Footer></Footer>
         </Fragment>
     );
 };
