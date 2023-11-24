8bf3df890dcc5984622625d01419f7099fd72d51
Matheus-OAMK <t2fema00@students.oamk.fi>
2023-11-24 21:23:22 +0200

    fixing some broken things that came with the layout change,  and adjusting the style on active parcel page

--- consumer_app/src/pages/ActiveParcelPage.jsx
+++ consumer_app/src/pages/ActiveParcelPage.jsx
@@ -1,16 +1,15 @@
 // ActiveParcelPage.js
 
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Info } from "lucide-react";
 
 const colorMapping = {
-    "In Route": "#00FF47",
-    "Awaiting drop-off": "#000AFF",
-    Delivered: "#FF9900",
-    "Prepared for Delivery": "#FFFFFF",
-    "At warehouse": "#EBFF00",
-    "Ready for pick up": "#9A9A9A",
+    "In Route": "status-orange",
+    "Awaiting drop-off": "status-gray",
+    Delivered: "status-green",
+    "Prepared for Delivery": "status-yellow",
+    "At warehouse": "status-white",
+    "Ready for pick up": "status-blue",
 };
 
 const ActiveParcel = [
@@ -55,49 +54,44 @@ const ActiveParcelPage = () => {
     };
 
     return (
-        <div className="relative flex min-h-screen flex-col">
-            <div className="my-10 ml-20 max-w-[600px]">
-                <h1 className="text-4xl font-normal text-white">
-                    Active Parcel
-                </h1>
-            </div>
-            <div className="max-container flex flex-col">
-                <div className="grid grid-cols-2 gap-x-20">
-                    {ActiveParcel.map((parcel) => (
-                        <div
-                            key={parcel.id}
-                            className="bg-dark-secondary border-slate-blue mb-10 h-[116px] w-[440px] cursor-pointer rounded-2xl border border-solid px-5 py-5 shadow-lg shadow-black/40"
-                            onClick={() => handleParcelClick(parcel.id)}
-                        >
-                            <div className="flex flex-row justify-between gap-x-10 text-lg">
-                                <div className="flex flex-col items-start">
-                                    <span className="mb-5">
-                                        ID: {parcel.id}
-                                    </span>
-                                    <span className="flex flex-row items-center">
-                                        <div
-                                            className="mr-2 h-2 w-2 rounded-full"
-                                            style={{
-                                                backgroundColor:
-                                                    colorMapping[parcel.status],
-                                            }}
-                                        ></div>
-                                        <span>{parcel.status}</span>
-                                    </span>
-                                </div>
-                                <div className="flex flex-col items-end">
-                                    <span className="mb-5">
-                                        Last Update: {parcel.lastUpdate}
-                                    </span>
-                                    <span className="flex flex-row items-center">
-                                        <span className="mr-1">More Info</span>
-                                        <Info size={12} />
-                                    </span>
-                                </div>
+        <div>
+            <h1 className="mb-9 pl-9 text-4xl font-normal text-white">
+                Active Parcel
+            </h1>
+
+            <div className="margin-x grid grid-cols-2 gap-x-20 gap-y-10">
+                {ActiveParcel.map((parcel) => (
+                    <div
+                        key={parcel.id}
+                        className="bg-dark-secondary border-slate-blue h-[116px] w-[440px] cursor-pointer 
+                            rounded-2xl border border-solid px-5 py-5 shadow-lg shadow-black/40
+                            transition-all duration-300 hover:scale-105"
+                        onClick={() => handleParcelClick(parcel.id)}
+                    >
+                        <div className="flex flex-row justify-between gap-x-10 text-lg">
+                            <div className="flex flex-col items-start">
+                                <span className="mb-5">ID: {parcel.id}</span>
+                                <span className="flex flex-row items-center">
+                                    <div
+                                        className={`mr-2 h-2 w-2 rounded-full ${`bg-${
+                                            colorMapping[parcel.status]
+                                        }`}`}
+                                    ></div>
+                                    <span>{parcel.status}</span>
+                                </span>
+                            </div>
+                            <div className="flex flex-col items-end">
+                                <span className="mb-5">
+                                    Last Update: {parcel.lastUpdate}
+                                </span>
+                                <span className="flex flex-row items-center hover:text-white transition-all duration-300">
+                                    <span className="mr-1 ">More Info</span>
+                                    <Info size={12} />
+                                </span>
                             </div>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
