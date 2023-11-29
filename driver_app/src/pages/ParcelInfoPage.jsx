1a09791e88a4f7c4fec11a4dce04e504d8845587
Matheus-OAMK <t2fema00@students.oamk.fi>
2023-11-29 18:33:28 +0200

    stashing changes to pull main branch

--- driver_app/src/pages/ParcelInfoPage.jsx
+++ driver_app/src/pages/ParcelInfoPage.jsx
@@ -1,7 +1,50 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 import Layout from "../components/layout/Layout";
 import { Button } from "../components";
 
 const ParcelInfoPage = () => {
+    const { parcel_id } = useParams();
+    const navigate = useNavigate();
+    const [parcelInfo, setParcelInfo] = useState({});
+
+    useEffect(() => {
+        const fetchParcelInfo = async () => {
+            const DRIVER_URL = import.meta.env.VITE_DRIVER_BACKEND_URL;
+
+            try {
+                const response = await axios.get(
+                    `${DRIVER_URL}/parcels/${parcel_id}`,
+                    { withCredentials: true },
+                );
+
+                const { status, data } = response.data;
+
+                if (status === "success") {
+                    setParcelInfo(data.parcel_info);
+                }
+            } catch (error) {
+                console.log(error);
+                navigate("/404", { replace: true });
+            }
+        };
+
+        fetchParcelInfo();
+    }, [parcel_id, navigate]);
+
+    const {
+        ship_to,
+        current_location,
+        length,
+        height,
+        width,
+        weight,
+        driver_accepted,
+        pickup_pin,
+        delivery_pin,
+    } = parcelInfo;
+
     return (
         <Layout>
             <h1 className="mb-9 text-4xl font-normal text-white">
@@ -27,14 +70,18 @@ const ParcelInfoPage = () => {
                         <p className=" w-36 ">To:</p>
                         <p className="text-white">Oulu</p>
                     </div>
-                    <div className="flex">
-                        <p className=" w-36 ">Pickup pin:</p>
-                        <p className="text-white">None</p>
-                    </div>
-                    <div className="flex">
-                        <p className=" w-36 ">Delivery pin:</p>
-                        <p className="text-white">43245</p>
-                    </div>
+                    {pickup_pin ? (
+                        <div className="flex">
+                            <p className=" w-36 ">Pickup pin:</p>
+                            <p className="text-white">None</p>
+                        </div>
+                    ) : null}
+                    {delivery_pin ? (
+                        <div className="flex">
+                            <p className=" w-36 ">Delivery pin:</p>
+                            <p className="text-white">43245</p>
+                        </div>
+                    ) : null}
                     <div className="flex">
                         <p className=" w-36 ">Weight:</p>
                         <p className="text-white">0.3 kg</p>
@@ -53,8 +100,9 @@ const ParcelInfoPage = () => {
                     </div>
                 </div>
             </div>
-            <div className="text-center mt-8">
-            <Button className="" text="Accept delivery" />
+
+            <div className="mt-8 text-center">
+                <Button className="" text="Accept delivery" />
             </div>
         </Layout>
     );
